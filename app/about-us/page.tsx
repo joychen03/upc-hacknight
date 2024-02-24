db4823d0eff112c2a14f70591a7ff51b187e1359
xuanyi <xuanyiqiu77@gmail.com>
2024-02-24 04:41:37 +0100

    asd

--- app/about-us/page.tsx
+++ app/about-us/page.tsx
@@ -1,24 +1,28 @@
-export default function AboutUsPage() {
-  const descripcion = "null";
-  return (
-    <div>
-      <div className="flex-col border-4 border-teal-700 p-8 max-w-xs rounded-br-lg  bg-teal-400">
-        <div className="m-10">
-          <img src="/image/red.jpg" alt="foto de perfil" />
-        </div>
+import mockAboutUs from "@/mock/about-us";
 
-        <div className="text-center ">
-          <h2 className="px-4 py-2 text-lg text-white mb-4 drop-shadow-2xl font-bold">
-            JIAFU
-          </h2>
-          <hr></hr>
-          <h3 className="text-white">
-            Jiafu is gay dauifsdbkjfbdskljflsdh flidsgfkljsdfsl dkubas uilfsdh
-            fluiusdhfsldkf sdlkufgsdl igsdlufsedgdefi.
-          </h3>
-          <hr></hr>
-        </div>
-      </div>
+export default function AboutUsPage() {
+  return (
+    <div className="grid grid-cols-3 grid-flow-row gap-x-20 gap-y-4 m-10">
+      {mockAboutUs.map((persona) => {
+        return (
+          <div key={persona.id}>
+            <div className="flex-col border-4 border-teal-700 p-8 rounded-br-lg ${persona.isGay ? bg-teal-600 : bg-pink-500}">
+              <div className="m-3 flex justify-center">
+                <img src={persona.image} alt="foto de perfil" />
+              </div>
+
+              <div className="text-center ">
+                <h2 className="px-4 py-2 text-lg text-white mb-4 drop-shadow-2xl font-bold">
+                  {persona.name}
+                </h2>
+                <hr></hr>
+                <h3 className="text-white">{persona.description}</h3>
+                <hr></hr>
+              </div>
+            </div>
+          </div>
+        );
+      })}
     </div>
   );
 }

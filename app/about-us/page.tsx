729438efec7bc42e8588995d78c7da3ba80f28cc
xuanyi <xuanyiqiu77@gmail.com>
2024-02-24 04:02:09 +0100

    asdasd

--- app/about-us/page.tsx
+++ app/about-us/page.tsx
@@ -1,8 +1,24 @@
 export default function AboutUsPage() {
+  const descripcion = "null";
   return (
     <div>
-      This is about us page
-      <p>Please develop it</p>
+      <div className="flex-col border-4 border-teal-700 p-8 max-w-xs rounded-br-lg  bg-teal-400">
+        <div className="m-10">
+          <img src="/image/red.jpg" alt="foto de perfil" />
+        </div>
+
+        <div className="text-center ">
+          <h2 className="px-4 py-2 text-lg text-white mb-4 drop-shadow-2xl font-bold">
+            JIAFU
+          </h2>
+          <hr></hr>
+          <h3 className="text-white">
+            Jiafu is gay dauifsdbkjfbdskljflsdh flidsgfkljsdfsl dkubas uilfsdh
+            fluiusdhfsldkf sdlkufgsdl igsdlufsedgdefi.
+          </h3>
+          <hr></hr>
+        </div>
+      </div>
     </div>
   );
 }
